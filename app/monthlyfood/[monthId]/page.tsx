64077582800b819d7a82b5d6e000d4f9e6ba89ed
손지은 <wiseag@naver.com>
2025-09-09 00:42:19 +0900

    test : api test

--- app/monthlyfood/[monthId]/page.tsx
+++ app/monthlyfood/[monthId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 const MonthlyFood = async () => {
   const res = await fetch(
-    `http://api.nongsaro.go.kr/service/monthFd/monthNewFdLst/?apiKey=20250908CQKM6OXUVP569KHIARHHA&thisYear=2018&thisMonth=1`,
+    `https://api.nongsaro.go.kr/service/monthFd/monthFdmtLst/?apiKey=20250908CQKM6OXUVP569KHIARHHA&thisYear=2018&thisMonth=1`,
     { method: "GET", headers: { "Content-Type": "application/json" } }
   );
   const data = res.json();
