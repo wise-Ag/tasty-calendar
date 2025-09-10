fb0d37868974e93e5df20cf05ba74b92bf515e4e
손지은 <wiseag@naver.com>
2025-09-10 23:50:25 +0900

    feat : xml to json

--- app/_api/index.ts
+++ app/_api/index.ts
@@ -2,21 +2,23 @@
 
 import { parseStringPromise } from "xml2js";
 let yearData: string[] = [];
+const BASE_URL = process.env.MONTHLY_FOOD_DATA_URL;
 
 export const getMFYear = async () => {
   try {
     const res = await fetch(
-      `${process.env.MONTHLY_FOOD_DATA_URL}/monthFdYearLst?apiKey=${process.env.MONTHLY_FOOD_DATA_KEY}`,
+      `${BASE_URL}/monthFdYearLst?apiKey=${process.env.MONTHLY_FOOD_DATA_KEY}`,
       { method: "GET", headers: { Accept: "application/xml" } }
     );
     const xml = await res.text();
-
     const json = await parseStringPromise(xml, {});
+
     const yearArr = json.response.body[0].items[0].item;
     yearData = [];
     yearArr.forEach((v: { year: Array<string> }) => {
       yearData.push(v.year[0]);
     });
+
     return yearData;
   } catch (err) {
     console.log("err", err);
