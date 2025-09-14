8abfb81927b3de82cbd88b66c6a0ce1067448caf
손지은 <wiseag@naver.com>
2025-09-14 23:15:09 +0900

    style : 이달의 음식, 레시피

--- app/_components/MonthFood.tsx
+++ app/_components/MonthFood.tsx
@@ -24,45 +24,59 @@ const MonthFood = ({ data }: { data: MonthlyFoodType[] }) => {
   }, [page]);
 
   return (
-    <div className={"flex justify-content-center items-center w-full"}>
-      {MAX_PAGE !== 0 && (
-        <button className={"cursor-pointer"} onClick={() => getPrevPage()}>
-          <Image
-            src={"/icons/left-arrow-icon.png"}
-            width={50}
-            height={50}
-            alt={"왼쪽 화살표"}
-          />
-        </button>
-      )}
-      <div className={"grid grid-cols-4 grid-rows-2 gap-10 p-2"}>
-        {foodData?.map((v) => {
-          if (!v) return;
-          const imgURL = getImagePath(v.rtnFileCours[0], v.rtnThumbFileNm[0]);
-          return (
-            <div key={v.cntntsNo[0]}>
+    <div>
+      <div className="justify-content-left text-3xl pl-10">이달의 음식</div>
+      <div className="flex flex-col justify-content-center items-center">
+        <div className="flex  justify-content-center items-center">
+          {MAX_PAGE !== 0 && (
+            <button
+              className={"cursor-pointer p-20"}
+              onClick={() => getPrevPage()}
+            >
               <Image
-                src={imgURL}
-                width={300}
-                height={200}
-                alt={"식재료 이미지"}
-                className={"rounded-xl cursor-pointer"}
+                src={"/icons/left-arrow-icon.png"}
+                width={50}
+                height={50}
+                alt={"왼쪽 화살표"}
               />
-              <div className={"text-center p-5"}>{v.fdmtNm[0]}</div>
-            </div>
-          );
-        })}
+            </button>
+          )}
+          <div className={"grid grid-cols-4 grid-rows-2 gap-15 "}>
+            {foodData?.map((v) => {
+              if (!v) return;
+              const imgURL = getImagePath(
+                v.rtnFileCours[0],
+                v.rtnThumbFileNm[0]
+              );
+              return (
+                <div key={v.cntntsNo[0]}>
+                  <Image
+                    src={imgURL}
+                    width={300}
+                    height={200}
+                    alt={"식재료 이미지"}
+                    className={"rounded-xl cursor-pointer "}
+                  />
+                  <div className={"text-center p-3"}>{v.fdmtNm[0]}</div>
+                </div>
+              );
+            })}
+          </div>
+          {MAX_PAGE !== 0 && (
+            <button
+              className={"cursor-pointer p-20"}
+              onClick={() => getNextPage()}
+            >
+              <Image
+                src={"/icons/right-arrow-icon.png"}
+                width={50}
+                height={50}
+                alt={"오른쪽 화살표"}
+              />
+            </button>
+          )}
+        </div>
       </div>
-      {MAX_PAGE !== 0 && (
-        <button className={"cursor-pointer"} onClick={() => getNextPage()}>
-          <Image
-            src={"/icons/right-arrow-icon.png"}
-            width={50}
-            height={50}
-            alt={"오른쪽 화살표"}
-          />
-        </button>
-      )}
     </div>
   );
 };
--- app/_components/MonthRecipe.tsx
+++ app/_components/MonthRecipe.tsx
@@ -19,36 +19,39 @@ const MonthRecipe = ({ data }: { data: RecipeType[] }) => {
   }, [page]);
 
   return (
-    <div className="flex flex-col justify-content-center items-center">
-      <div className={"grid grid-cols-3 grid-rows-2 gap-4 p-4 "}>
-        {recipeData?.map((v) => {
-          if (!v) return;
-          const imgURL = getImagePath(v.rtnFileCours[0], v.rtnThumbFileNm[0]);
-          return (
-            <div
-              key={v.cntntsNo[0]}
-              className="flex flex-col justify-content-center items-center"
-            >
-              <Image
-                src={imgURL}
-                width={300}
-                height={200}
-                alt={"식재료 이미지"}
-                className="rounded-xl cursor-pointer"
-              />
-              <div>{v.fdNm[0]}</div>
-            </div>
-          );
-        })}
+    <div className="bg-orange-100">
+      <div className="flex text-3xl pl-10 pt-20">이달의 레시피</div>
+      <div className="flex flex-col justify-content-center items-center">
+        <div className={"grid grid-cols-3 grid-rows-2 gap-15 p-4 "}>
+          {recipeData?.map((v) => {
+            if (!v) return;
+            const imgURL = getImagePath(v.rtnFileCours[0], v.rtnThumbFileNm[0]);
+            return (
+              <div
+                key={v.cntntsNo[0]}
+                className="flex flex-col justify-content-center items-center"
+              >
+                <Image
+                  src={imgURL}
+                  width={400}
+                  height={200}
+                  alt={"식재료 이미지"}
+                  className="rounded-xl cursor-pointer"
+                />
+                <div className="pt-3">{v.fdNm[0]}</div>
+              </div>
+            );
+          })}
+        </div>
+        {MAX_PAGE !== page && (
+          <button
+            className="cursor-pointer w-60 rounded-xl bg-orange-300 p-2 pr-5 pl-5 text-white font-size-20px"
+            onClick={() => getMoreData()}
+          >
+            더보기
+          </button>
+        )}
       </div>
-      {MAX_PAGE !== page && (
-        <button
-          className="cursor-pointer w-60 rounded-xl bg-orange-300 p-2 pr-5 pl-5 text-white font-size-20px"
-          onClick={() => getMoreData()}
-        >
-          더보기
-        </button>
-      )}
     </div>
   );
 };
