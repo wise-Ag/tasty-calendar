7d6614764b0436b47ffe202c749fa53c95c3904f
손지은 <wiseag@naver.com>
2025-09-26 14:01:28 +0900

    feat : 페이지네이션 슬라이딩 애니메이션

--- app/_components/MonthFood.tsx
+++ app/_components/MonthFood.tsx
@@ -9,27 +9,40 @@ import clsx from "clsx";
 
 const MonthFood = ({ data }: { data: MonthlyFoodType[] }) => {
   const PAGE_SIZE = 8;
-  const [foodData, setFoodData] = useState<MonthlyFoodType[]>(
-    data.slice(0, PAGE_SIZE)
-  );
+
+  const [foodList, setFoodList] = useState<MonthlyFoodType[][]>();
   const [page, setPage] = useState<number>(0);
+
+  const [prevPage, setPrevPage] = useState<number>(0);
   const [direction, setDirection] = useState<"left" | "right" | "none">("none");
 
-  const MAX_PAGE = Math.floor(data.length / PAGE_SIZE);
+  const MAX_PAGE =
+    data.length % PAGE_SIZE
+      ? Math.floor(data.length / PAGE_SIZE)
+      : data.length / PAGE_SIZE - 1;
 
   const getNextPage = () => {
+    setPrevPage(page);
     page === MAX_PAGE ? setPage(0) : setPage(page + 1);
     setDirection("right");
   };
 
   const getPrevPage = () => {
+    setPrevPage(page);
     page === 0 ? setPage(MAX_PAGE) : setPage(page - 1);
     setDirection("left");
   };
 
   useEffect(() => {
-    setFoodData(data.slice(page * PAGE_SIZE, page * PAGE_SIZE + PAGE_SIZE));
-  }, [page]);
+    const foodListData = [];
+    for (let pageIndex = 0; pageIndex < MAX_PAGE + 1; pageIndex++) {
+      foodListData.push(
+        data.slice(pageIndex * PAGE_SIZE, pageIndex * PAGE_SIZE + PAGE_SIZE)
+      );
+    }
+    setFoodList(foodListData);
+    console.log(foodListData);
+  }, []);
 
   return (
     <div>
@@ -37,10 +50,10 @@ const MonthFood = ({ data }: { data: MonthlyFoodType[] }) => {
         이달의 음식
       </div>
       <div className="flex flex-col justify-content-center items-center">
-        <div className="flex  justify-content-center items-center">
+        <div className="flex w-full h-full justify-content-center items-center">
           {MAX_PAGE !== 0 && (
             <button
-              className={"cursor-pointer m-10"}
+              className={"cursor-pointer m-10 w-10"}
               onClick={() => getPrevPage()}
             >
               <Image
@@ -51,42 +64,59 @@ const MonthFood = ({ data }: { data: MonthlyFoodType[] }) => {
               />
             </button>
           )}
-          <div
-            key={page}
-            className={clsx(
-              `grid sm:grid-cols-1 sm:grid-row-1 md:grid-cols-3 lg:grid-cols-4 lg:grid-rows-2 gap-15`,
-              direction === "none"
-                ? ""
-                : direction === "right"
-                ? "animate-slide-right"
-                : "animate-slide-left"
-            )}
-          >
-            {foodData?.map((v) => {
-              if (!v) return;
-              const imgURL = v.rtnThumbFileNm[0]
-                ? getImagePath(v.rtnFileCours[0], v.rtnThumbFileNm[0])
-                : "/icons/no-food-img.png";
+          <div className="flex-1 relative w-full h-200 overflow-hidden">
+            {foodList?.map((list, i) => {
+              let position = "-translate-x-full";
+
+              if (page === i)
+                position =
+                  direction === "left"
+                    ? "animate-slide-left"
+                    : "animate-slide-right";
+              if (prevPage === i)
+                position =
+                  direction === "right"
+                    ? "animate-slideout-left"
+                    : "animate-slideout-right";
+
               return (
-                <div key={v.cntntsNo[0]}>
-                  <div className="relative w-70 h-60">
-                    <Link href={`/foodDetail/${v.cntntsNo}`}>
-                      <Image
-                        src={imgURL}
-                        fill
-                        alt={"식재료 이미지"}
-                        className={"rounded-xl cursor-pointer sm:width-full"}
-                      />
-                    </Link>
-                  </div>
-                  <div className={"text-center p-3"}>{v.fdmtNm[0]}</div>
+                <div
+                  key={i}
+                  className={clsx(
+                    `absolute inset-0 grid sm:grid-cols-1 sm:grid-row-1 md:grid-cols-3 lg:grid-cols-4 lg:grid-rows-2 gap-15 `,
+                    direction === "none" && page === i ? "" : position
+                  )}
+                >
+                  {list?.map((v) => {
+                    if (!v) return;
+                    const imgURL = v.rtnThumbFileNm[0]
+                      ? getImagePath(v.rtnFileCours[0], v.rtnThumbFileNm[0])
+                      : "/icons/no-food-img.png";
+                    return (
+                      <div key={v.cntntsNo[0]}>
+                        <div className="relative w-full aspect-[4/3]">
+                          <Link href={`/foodDetail/${v.cntntsNo}`}>
+                            <Image
+                              src={imgURL}
+                              fill
+                              alt={"식재료 이미지"}
+                              className={
+                                "rounded-xl cursor-pointer sm:width-full"
+                              }
+                            />
+                          </Link>
+                        </div>
+                        <div className={"text-center p-3"}>{v.fdmtNm[0]}</div>
+                      </div>
+                    );
+                  })}
                 </div>
               );
             })}
           </div>
           {MAX_PAGE !== 0 && (
             <button
-              className={"cursor-pointer m-10"}
+              className={"cursor-pointer m-10 w-10"}
               onClick={() => getNextPage()}
             >
               <Image
